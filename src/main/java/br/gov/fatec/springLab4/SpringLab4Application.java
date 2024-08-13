b65602a5935e0efa3aef7cdfd05d9e6f0cff4a10
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2024-08-13 20:38:41 -0300

    first commit

---
+++ src/main/java/br/gov/fatec/springLab4/SpringLab4Application.java
@@ -0,0 +1,13 @@
+package br.gov.fatec.springLab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/fatec/springLab4/SpringLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.fatec.springLab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
